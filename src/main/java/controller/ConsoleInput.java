d428007f29e09da702deee30f0341e5690b725a3
Andrea <chiappi.andrea@gmail.com>
2025-09-29 23:42:42 +0200

    aggiunta primi javadocs al controller

--- src/main/java/controller/ConsoleInput.java
+++ src/main/java/controller/ConsoleInput.java
@@ -2,10 +2,17 @@ package controller;
 
 import java.util.Scanner;
 
+/**
+ * Implementazione di {@link InputPort} che legge l'input da console utilizzando un {@link Scanner}
+ */
 public class ConsoleInput implements InputPort {
 
     private final Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Legge una riga di testo della console.
+     * @return la riga inserita dall'utente, senza il carattere newline finale.
+     */
     @Override
     public String getInput() {
         return scanner.nextLine();
--- src/main/java/controller/InputPort.java
+++ src/main/java/controller/InputPort.java
@@ -1,5 +1,9 @@
 package controller;
 
+/**
+ * Adattatore che permette il passaggio dell'input al core.
+ * @param <T> generico.
+ */
 public interface InputPort<T> {
     T getInput();
 }
